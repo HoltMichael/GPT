c683908a7579e77d0c633dbf8701732cc106446e
HoltMichael <michaelholt@hotmail.co.uk>
2023-01-26 17:37:04 +0000

    Styling gpt response lwc

---
+++ force-app/main/default/lwc/gptResponseItem/gptResponseItem.js
@@ -0,0 +1,17 @@
+import { LightningElement, api } from 'lwc';
+
+export default class GptResponseItem extends LightningElement {
+    @api text;
+    @api prompt;
+
+    showPrompt = false;
+    buttonIcon = "utility:chevrondown";
+
+    handleTogglePrompt(){
+        this.showPrompt = !this.showPrompt;
+        this.buttonIcon = this.buttonIcon == "utility:chevrondown" ? "utility:chevronup" : "utility:chevrondown";
+    }
+
+
+
+}
--- force-app/main/default/lwc/gptResponseViewer/gptResponseViewer.js
+++ force-app/main/default/lwc/gptResponseViewer/gptResponseViewer.js
@@ -9,13 +9,15 @@ export default class GptResponseViewer extends LightningElement {
 
     @wire(getRelatedListRecords, {
         parentRecordId: '$recordId',
-        relatedListId: 'MHolt__ChatGPT_Responses__r',
-        fields: ['MHolt__Response__c','Id']
+        relatedListId: 'MHolt__GPT_Responses__r',
+        fields: ['MHolt__GPT_Response__c.MHolt__Response__c', 'MHolt__GPT_Response__c.id', 'MHolt__GPT_Response__c.MHolt__Input__c'] 
         // sortBy: ['Contact.Name']
     })response({ error, data }) {
         if (data) {
             console.log(data);
             this.responses = data.records;
+            console.log('Response:');
+            console.log(this.responses[0]);
             this.error = undefined;
         } else if (error) {
             console.log(error);
